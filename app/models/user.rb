245c80efce3471a10326c84f4371bd64dda6b8d4
nikita singh <nikitaa_singh@yahoo.co.in>
2013-01-03 14:10:40 +0530

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable,:registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+validates :username,  :presence => true,:uniqueness => true
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :username, :password, :password_confirmation, :remember_me, :role_ids
+  attr_accessible :email, :username, :password, :password_confirmation, :remember_me, :role_ids, :username
 
   def role?(role)
     return !!self.roles.find_by_name(role.to_s)
