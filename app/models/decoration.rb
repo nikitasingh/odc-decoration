96d04b5b110192ee35c77c2c832ca68ca4352373
nikita singh <nikitaa_singh@yahoo.co.in>
2013-01-03 11:36:15 +0530

    Some ui changes

--- app/models/decoration.rb
+++ app/models/decoration.rb
@@ -1,8 +1,9 @@
 class Decoration < ActiveRecord::Base
 	attr_accessible :name, :year, :zone_id 
 	validates :name,  :presence => true,:uniqueness => true
-validates_attachment_content_type :expense, :content_type => ['application/excel','application/vnd.ms-excel','application/
-vnd.msexcel']
+	validates :year,  :presence => true
+	validates_attachment_content_type :expense, :content_type => ['application/excel','application/vnd.ms-excel','application/
+	vnd.msexcel']
 	attr_accessible :expense
     has_attached_file :expense
     has_many:tasks
